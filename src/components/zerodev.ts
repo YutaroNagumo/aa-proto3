15ad5bf591b753144ddab656450bd9301c6987a8
南雲悠太郎 <yutaro@nagumoyuutarounoMac-Studio.local>
2023-07-24 10:26:10 +0900

    7

--- src/components/zerodev.ts
+++ src/components/zerodev.ts
@@ -1,14 +1,15 @@
 const { Contract, Wallet } = require('ethers')
 const { getZeroDevSigner } = require('@zerodevapp/sdk')
+const ethers = require('ethers');
 
 const projectId = "65c3edab-389f-4b50-a004-f95eed834ed6"
 const wallet = new Wallet("0x8e9a8e453feac836634af17b3efe4fb3be1f50c263c48a9bcb352a47015f6e7e")
 
-const contractAddress = '0x34bE7f35132E97915633BC1fc020364EA5134863'
-const contractABI = [
-  'function mint(address _to) public',
-  'function balanceOf(address owner) external view returns (uint256 balance)'
-]
+// const contractAddress = '0x34bE7f35132E97915633BC1fc020364EA5134863'
+// const contractABI = [
+//   'function mint(address _to) public',
+//   'function balanceOf(address owner) external view returns (uint256 balance)'
+// ]
 
 const sendOperationAndGetHash_z = async () => {
   const signer = await getZeroDevSigner({
@@ -16,23 +17,23 @@ const sendOperationAndGetHash_z = async () => {
     owner: wallet,
   })
 
-  //終了時間get
+  //開始時間get
   const startTime = Date.now();
   console.log(startTime);
 
   const address = await signer.getAddress()
   console.log('My address:', address)
 
-  const nftContract = new Contract(contractAddress, contractABI, signer)
-
-  const receipt = await nftContract.mint(address)
-  // const receipt = await signer.encodeExecute([{
-  //   to: "0xE15967C015C68c7E26F606411f9174376002aDcC",
-  //   data: "0x",
-  //   value: 0.000002,
-  // }]);
-  // await receipt.wait()
-  // console.log(`NFT balance: ${await nftContract.balanceOf(address)}`)
+  // const nftContract = new Contract(contractAddress, contractABI, signer)
+  // const receipt = await nftContract.mint(address)
+  
+  const receipt = await signer.sendTransaction({
+    gasLimit: "0x55555",
+    to: '0x105286463682bF73D0E753f471dBC5E85d098181',
+    data: "0x",
+    value: ethers.utils.parseEther('0.000002'),
+  });
+  await receipt.wait()
 
   //終了時間get
   const endTime = Date.now();
@@ -42,7 +43,6 @@ const sendOperationAndGetHash_z = async () => {
 
   //Tx Hash獲得
   console.log("UOHash: ",receipt.hash);
-  //jiffyscanの調子が悪いので一旦スクレイピング中止
   const response = await fetch(`/api/scrapeJiffy?hash=${receipt.hash}`);
   const data = await response.json();
   console.log("data: ",data);
